c1935128528303a48dc6a93fa7bd6da9e9a6de49
Fabio Carvalho <fabiocarvalho@saovicente.sp.gov.br>
2025-05-20 13:40:55 -0300

    Corrigindo dados

--- src/app/apresentacao/apresentacao.component.ts
+++ src/app/apresentacao/apresentacao.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './apresentacao.component.css'
 })
 export class ApresentacaoComponent {
-
+  nome = "Cliente"
 }
--- src/app/area-financeira/area-financeira.component.ts
+++ src/app/area-financeira/area-financeira.component.ts
@@ -18,63 +18,9 @@ export class AreaFinanceiraComponent {
     },0)
   });
 
-  transacoes = signal<Transacao[]>([
-    {
-      id: '5',
-      nome: '',
-      tipo: TipoTransacao.SAQUE,
-      valor: 200,
-      data: new Date('2025-02-20T00:00'),
-      conta: 'Switch Bank'
-    },
-    {
-      id: '4',
-      nome: 'Almoço',
-      tipo: TipoTransacao.SAQUE,
-      valor: 40,
-      data: new Date('2025-01-15T00:00'),
-      conta: 'Bytebank'
-    },
-    {
-      id: '3',
-      nome: '',
-      tipo: TipoTransacao.DEPOSITO,
-      valor: 400,
-      data: new Date('2025-01-10T00:00'),
-      conta: 'Bytebank'
-    },
-    {
-      id: '2',
-      nome: 'Freela (2ª parte)',
-      tipo: TipoTransacao.DEPOSITO,
-      valor: 200,
-      data: new Date('2024-10-01T00:00'),
-      conta: 'Anybank'
-    },
-    {
-      id: '1',
-      nome: 'Freela (1ª parte)',
-      tipo: TipoTransacao.DEPOSITO,
-      valor: 100,
-      data: new Date('2024-10-01T00:00'),
-      conta: 'Anybank'
-    },
-  ]);
+  transacoes = signal<Transacao[]>([]);
 
-  contasComSaldoInicial = signal<Conta[]>([
-    {
-      nome: 'Anybank',
-      saldo: 1000,
-    },
-    {
-      nome: 'Bytebank',
-      saldo: 0,
-    },
-    {
-      nome: 'Switch Bank',
-      saldo: 0,
-    },
-  ]);
+  contasComSaldoInicial = signal<Conta[]>([]);
 
   contas = computed(() => {
     return this.contasComSaldoInicial().map((conta) => {
